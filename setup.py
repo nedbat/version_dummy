2dc05823ac1fd3aea8f1ca5abdadc31bc1b50b36
Ned Batchelder <ned@nedbatchelder.com>
2015-10-22 09:57:54 -0400

    Update setup.py also

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="version_dummy",
-    version="2.0",
+    version="2.0.001",
     author="Ned Batchelder",
     author_email="ned@nedbatchelder.com",
     url="http://nedbatchelder.com",
