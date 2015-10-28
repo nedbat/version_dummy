9cbf98bef1a18a8c30055ab054c308f2d1d3faa8
Ned Batchelder <ned@nedbatchelder.com>
2015-10-28 14:23:58 -0400

    Lots more info about the installation, and e-then-regular-with-version

--- experiment.sh
+++ experiment.sh
@@ -4,8 +4,24 @@ V1_SHA="da8cfc943a8acb3140349a81e96ea52430bf0e02"
 V2_SHA="ca10cfd07afdce20d61249d5a8e306cf64a3b2b5"
 
 cat > show_version.py << EOF_PY
-import version_dummy
-print('*** Version dummy is {0}, at {1}'.format(version_dummy.VERSION, version_dummy.__file__))
+from distutils.sysconfig import get_python_lib
+import os, os.path
+
+try:
+    import version_dummy
+    print("*** Version dummy is {0}, at {1}".format(version_dummy.VERSION, version_dummy.__file__))
+except ImportError:
+    print("*** Version dummy isn't available")
+
+interesting = [fn for fn in os.listdir(get_python_lib()) if "dummy" in fn]
+for fn in interesting:
+    fn = os.path.join(get_python_lib(), fn)
+    if os.path.isfile(fn):
+        with open(fn) as f:
+            contents = f.read().strip()
+    else:
+        contents = os.listdir(fn)
+    print("    {0}\n        --> {1}".format(fn, contents))
 EOF_PY
 
 echo
@@ -20,7 +36,9 @@ pip install -r requirements.txt
 python show_version.py
 pip freeze | grep dummy
 pip uninstall -y version_dummy
+python show_version.py
 pip uninstall -y version_dummy
+python show_version.py
 
 echo
 echo "==== version_dummy with mismatched name ===="
@@ -33,7 +51,9 @@ pip install -r requirements.txt
 python show_version.py
 pip freeze | grep dummy
 pip uninstall -y version_dummy
+python show_version.py
 pip uninstall -y version_dummy
+python show_version.py
 
 echo
 echo "==== version_dummy -e ===="
@@ -46,7 +66,9 @@ pip install -r requirements.txt
 python show_version.py
 pip freeze | grep dummy
 pip uninstall -y version_dummy
+python show_version.py
 pip uninstall -y version_dummy
+python show_version.py
 
 echo
 echo "==== version_dummy with version ===="
@@ -59,10 +81,12 @@ pip install -r requirements.txt
 python show_version.py
 pip freeze | grep dummy
 pip uninstall -y version_dummy
+python show_version.py
 pip uninstall -y version_dummy
+python show_version.py
 
 echo
-echo "==== -e then regular ===="
+echo "==== -e then regular without version ===="
 echo "-e git+https://github.com/nedbat/version_dummy@$V1_SHA#egg=version_dummy" > requirements.txt
 pip install -r requirements.txt
 python show_version.py
@@ -72,7 +96,24 @@ pip install -r requirements.txt
 python show_version.py
 pip freeze | grep dummy
 pip uninstall -y version_dummy
+python show_version.py
 pip uninstall -y version_dummy
+python show_version.py
+
+echo
+echo "==== -e then regular with version ===="
+echo "-e git+https://github.com/nedbat/version_dummy@$V1_SHA#egg=version_dummy" > requirements.txt
+pip install -r requirements.txt
+python show_version.py
+pip freeze | grep dummy
+echo "git+https://github.com/nedbat/version_dummy@$V2_SHA#egg=version_dummy==2.0" > requirements.txt
+pip install -r requirements.txt
+python show_version.py
+pip freeze | grep dummy
+pip uninstall -y version_dummy
+python show_version.py
+pip uninstall -y version_dummy
+python show_version.py
 
 echo
 echo "==== regular then -e ===="
@@ -85,6 +126,8 @@ pip install -r requirements.txt
 python show_version.py
 pip freeze | grep dummy
 pip uninstall -y version_dummy
+python show_version.py
 pip uninstall -y version_dummy
+python show_version.py
 
 rm requirements.txt show_version.py
